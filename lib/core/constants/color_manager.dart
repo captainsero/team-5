d0c86ce7672a811be7ee1611c63b065f74aaa728
Your Name <mohamedmedo2282004a@gmail.com>
2026-03-07 01:20:09 +0200

    chore(core):add core folder setup

---
+++ lib/core/constants/color_manager.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+sealed class AppColors {
+  static Color primary = const Color(0xff02369C);
+  static Color lightBlue = const Color(0xffEDEFF3);
+  static Color grey = const Color(0xff535353);
+  static Color error = const Color(0xffCC1010);
+  static Color black = const Color(0xff0F0F0F);
+  static Color sucess = const Color(0xff11CE19);
+  static Color lightGreen = const Color(0xffCAF9CC);
+  static Color lightRed = const Color(0xffF8D2D2);
+  static Color placeholder = const Color(0xffA6A6A6);
+  static Color white = Colors.white;
+
+}
---
+++ lib/core/constants/values_manager.dart
@@ -0,0 +1,52 @@
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+class AppMargin {
+  static double get m8 => 8.0.w;
+  static double get m12 => 12.0.w;
+  static double get m16 => 16.0.w;
+  static double get m20 => 20.0.w;
+}
+
+class AppPadding {
+  static double get p0 => 0.0.w;
+  static double get p2 => 2.0.w;
+  static double get p4 => 4.0.w;
+  static double get p8 => 8.0.w;
+  static double get p12 => 12.0.w;
+  static double get p16 => 16.0.w;
+  static double get p20 => 20.0.w;
+  static double get p30 => 30.0.w;
+  static double get p40 => 40.0.w;
+}
+
+class AppSize {
+  static double get s0 => 0.0.w;
+  static double get s1 => 1.0.w;
+  static double get s2 => 2.0.w;
+  static double get s1_5 => 1.5.w;
+  static double get s3 => 3.w;
+  static double get s4 => 4.0.w;
+  static double get s6 => 6.0.w;
+  static double get s8 => 8.0.w;
+  static double get s10 => 10.0.w;
+  static double get s12 => 12.0.w;
+  static double get s14 => 14.0.w;
+  static double get s16 => 16.0.w;
+  static double get s20 => 20.0.w;
+  static double get s25 => 25.0.w;
+  static double get s30 => 30.0.w;
+  static double get s40 => 40.0.w;
+  static double get s50 => 50.0.w;
+  static double get s100 => 100.0.w;
+  static double get s200 => 200.0.w;
+}
+
+class RadiusSize {
+  static double get r4 => 4.0.r;
+  static double get r8 => 8.0.r;
+  static double get r12 => 12.0.r;
+  static double get r16 => 16.0.r;
+  static double get r20 => 20.0.r;
+  static double get r30 => 30.0.r;
+  static double get r70 => 70.0.r;
+  static double get r100 => 100.0.r;
+}
