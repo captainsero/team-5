31f4c34b07f8761e424078b19420972f4dc0a6ba
Yasser Samir <yasoo976098@gmail.com>
2026-03-08 17:54:38 +0200

    feat(forgetPassword): add ForgetPassword Screen

---
+++ lib/features/auth/forget_password/presentation/screens/forget_pass_view.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:team_5_examapp/core/constants/values_manager.dart';
+
+class ForgetPassView extends StatefulWidget {
+  const ForgetPassView({super.key});
+
+  @override
+  State<ForgetPassView> createState() => _ForgetPassViewState();
+}
+
+class _ForgetPassViewState extends State<ForgetPassView> {
+  final _formKey = GlobalKey<FormState>();
+  final emailController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Icon(Icons.arrow_back_ios),
+        title: Text("Password"),
+      ),
+      body: Form(
+        key: _formKey,
+        child: SingleChildScrollView(
+          padding: EdgeInsets.all(AppPadding.p20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+
+            children: [
+              Text(
+                "Forget Password",
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+
+              SizedBox(height: AppSize.s16),
+
+              Text(
+                "Please enter your email associated to your account",
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.bodyLarge,
+              ),
+
+              SizedBox(height: AppSize.s30),
+
+              TextFormField(
+                controller: TextEditingController(),
+                decoration: InputDecoration(
+                  hintText: "Enter your email",
+                  labelText: "Email",
+                ),
+              ),
+
+              SizedBox(height: AppSize.s30),
+
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text("Continue"),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
