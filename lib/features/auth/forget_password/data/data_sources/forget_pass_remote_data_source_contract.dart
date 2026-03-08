a71398f707b7bfb6366628b6fae64e4cf23d6b13
Yasser Samir <yasoo976098@gmail.com>
2026-03-08 16:16:48 +0200

    feat(forget password): update repository contracts and implementation to return ForgetPasswordResponse

--- lib/features/auth/forget_password/data/data_sources/forget_pass_remote_data_source_contract.dart
+++ lib/features/auth/forget_password/data/data_sources/forget_pass_remote_data_source_contract.dart
@@ -1,10 +1,11 @@
 import 'package:team_5_examapp/config/base_response/base_response.dart';
 import 'package:team_5_examapp/features/auth/forget_password/data/models/reset_pass_dto.dart';
+import 'package:team_5_examapp/features/auth/forget_password/data/models/responses/forget_password_response.dart';
 
 abstract class ForgetPassRemoteDataSourceContract {
-  Future<BaseResponse<void>> forgetPassword({required String email});
-  Future<BaseResponse<void>> confirmValidationCode({required String resetCode});
-  Future<BaseResponse<void>> resetPassword({
+  Future<BaseResponse<ForgetPasswordResponse>> forgetPassword({required String email});
+  Future<BaseResponse<ForgetPasswordResponse>> confirmValidationCode({required String resetCode});
+  Future<BaseResponse<ForgetPasswordResponse>> resetPassword({
     required ResetPassDto resetPassDto,
   });
 }
--- lib/features/auth/forget_password/data/repo/forget_pass_repo_impl.dart
+++ lib/features/auth/forget_password/data/repo/forget_pass_repo_impl.dart
@@ -2,6 +2,7 @@ import 'package:injectable/injectable.dart';
 import 'package:team_5_examapp/config/base_response/base_response.dart';
 import 'package:team_5_examapp/features/auth/forget_password/data/data_sources/forget_pass_remote_data_source_contract.dart';
 import 'package:team_5_examapp/features/auth/forget_password/data/models/reset_pass_dto.dart';
+import 'package:team_5_examapp/features/auth/forget_password/data/models/responses/forget_password_response.dart';
 import 'package:team_5_examapp/features/auth/forget_password/domain/repo/forget_pass_repo_contract.dart';
 
 @Injectable(as: ForgetPassRepoContract)
@@ -11,44 +12,58 @@ class ForgetPassRepoImpl implements ForgetPassRepoContract {
   ForgetPassRepoImpl({required this.forgetPassRemoteDataSourceContract});
 
   @override
-  Future<BaseResponse<void>> forgetPassword({required String email}) async {
+  Future<BaseResponse<ForgetPasswordResponse>> forgetPassword({
+    required String email,
+  }) async {
     final response = await forgetPassRemoteDataSourceContract.forgetPassword(
       email: email,
     );
     switch (response) {
-      case SucceessBaseResponse<void>():
-        return SucceessBaseResponse<void>(data: null);
-      case ErrorBaseResponse<void>():
-        return ErrorBaseResponse<void>(errorMessage: response.errorMessage);
+      case SucceessBaseResponse<ForgetPasswordResponse>():
+        return SucceessBaseResponse<ForgetPasswordResponse>(
+          data: response.data,
+        );
+      case ErrorBaseResponse<ForgetPasswordResponse>():
+        return ErrorBaseResponse<ForgetPasswordResponse>(
+          errorMessage: response.errorMessage,
+        );
     }
   }
 
   @override
-  Future<BaseResponse<void>> confirmValidationCode({
+  Future<BaseResponse<ForgetPasswordResponse>> confirmValidationCode({
     required String resetCode,
   }) async {
     final response = await forgetPassRemoteDataSourceContract
         .confirmValidationCode(resetCode: resetCode);
     switch (response) {
-      case SucceessBaseResponse<void>():
-        return SucceessBaseResponse<void>(data: null);
-      case ErrorBaseResponse<void>():
-        return ErrorBaseResponse<void>(errorMessage: response.errorMessage);
+      case SucceessBaseResponse<ForgetPasswordResponse>():
+        return SucceessBaseResponse<ForgetPasswordResponse>(
+          data: response.data,
+        );
+      case ErrorBaseResponse<ForgetPasswordResponse>():
+        return ErrorBaseResponse<ForgetPasswordResponse>(
+          errorMessage: response.errorMessage,
+        );
     }
   }
 
   @override
-  Future<BaseResponse<void>> resetPassword({
+  Future<BaseResponse<ForgetPasswordResponse>> resetPassword({
     required ResetPassDto resetPassDto,
   }) async {
     final response = await forgetPassRemoteDataSourceContract.resetPassword(
       resetPassDto: resetPassDto,
     );
     switch (response) {
-      case SucceessBaseResponse<void>():
-        return SucceessBaseResponse<void>(data: null);
-      case ErrorBaseResponse<void>():
-        return ErrorBaseResponse<void>(errorMessage: response.errorMessage);
+      case SucceessBaseResponse<ForgetPasswordResponse>():
+        return SucceessBaseResponse<ForgetPasswordResponse>(
+          data: response.data,
+        );
+      case ErrorBaseResponse<ForgetPasswordResponse>():
+        return ErrorBaseResponse<ForgetPasswordResponse>(
+          errorMessage: response.errorMessage,
+        );
     }
   }
 }
