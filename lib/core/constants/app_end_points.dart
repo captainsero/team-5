099d9b592f0d00c2b10fe60d0cafe4f8a3fa4b03
Your Name <mohamedmedo2282004a@gmail.com>
2026-03-14 01:59:54 +0200

    feat(login) : complete login feature

---
+++ lib/core/constants/app_end_points.dart
@@ -0,0 +1,6 @@
+abstract class AppEndPoints {
+  static const String baseUrl = "https://exam.elevateegy.com/api/v1";
+  static const String forgetPassword = "$baseUrl/auth/forgotPassword";
+  static const String verifyResetCode = "$baseUrl/auth/verifyResetCode";
+  static const String resetPassword = "$baseUrl/auth/resetPassword";
+}
