a4e59df2a8dd06131b743050182fb264c2159fa6
Yasser Samir <yasoo976098@gmail.com>
2026-03-27 08:28:21 +0200

    fix(api): make data source reture a list of questionModel

--- lib/features/questions/api/data_sources/questions_remote_data_source_impl.dart
+++ lib/features/questions/api/data_sources/questions_remote_data_source_impl.dart
@@ -2,7 +2,7 @@ import 'package:injectable/injectable.dart';
 import 'package:team_5_examapp/config/base_response/base_response.dart';
 import 'package:team_5_examapp/features/questions/api/questions_api_client/questions_api_client.dart';
 import 'package:team_5_examapp/features/questions/data/data_sources/questions_remote_data_source_contract.dart';
-import 'package:team_5_examapp/features/questions/data/models/responses/questions_response.dart';
+import 'package:team_5_examapp/features/questions/data/models/question_model.dart';
 
 @Injectable(as: QuestionsRemoteDataSourceContract)
 class QuestionsRemoteDataSourceImpl
@@ -11,7 +11,7 @@ class QuestionsRemoteDataSourceImpl
 
   QuestionsRemoteDataSourceImpl({required this.questionsApiClient});
   @override
-  Future<BaseResponse<QuestionsResponse>> getAllQuestionsOnExam({
+  Future<BaseResponse<List<QuestionModel>>> getAllQuestionsOnExam({
     required String token,
     required String examId,
   }) async {
@@ -22,10 +22,10 @@ class QuestionsRemoteDataSourceImpl
         examId: examId,
       );
       print("Success: ${response.questions}");
-      return SuccessBaseResponse<QuestionsResponse>(data: response);
+      return SuccessBaseResponse<List<QuestionModel>>(data: response.questions);
     } catch (e) {
       print("Error: $e");
-      return ErrorBaseResponse<QuestionsResponse>(error: e);
+      return ErrorBaseResponse<List<QuestionModel>>(error: e);
     }
   }
 }
--- lib/features/questions/data/data_sources/questions_remote_data_source_contract.dart
+++ lib/features/questions/data/data_sources/questions_remote_data_source_contract.dart
@@ -1,8 +1,8 @@
 import 'package:team_5_examapp/config/base_response/base_response.dart';
-import 'package:team_5_examapp/features/questions/data/models/responses/questions_response.dart';
+import 'package:team_5_examapp/features/questions/data/models/question_model.dart';
 
 abstract class QuestionsRemoteDataSourceContract {
-  Future<BaseResponse<QuestionsResponse>> getAllQuestionsOnExam({
+  Future<BaseResponse<List<QuestionModel>>> getAllQuestionsOnExam({
     required String token,
     required String examId,
   });
