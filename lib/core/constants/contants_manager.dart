bce8c4095c76b29356006354198ce0c672766061
Yasser Samir <yasoo976098@gmail.com>
2026-03-30 19:01:21 +0200

    chore(core): add hive keys to the constants manager

--- lib/core/constants/contants_manager.dart
+++ lib/core/constants/contants_manager.dart
@@ -2,3 +2,8 @@ abstract class AppConstants {
   static const double scoreBarWidth = 0.12;
   static const double scoreBarAngle = 280;
 }
+
+abstract class HiveKeys {
+  static const String answersBoxName = 'answersBox';
+  static const String checkAnswersBoxName = 'checkAnswersBox';
+}
