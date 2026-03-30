82be77f213c5c30dbbbb3cd08a3a93e13e03fdba
Yasser Samir <yasoo976098@gmail.com>
2026-03-30 19:00:09 +0200

    chore(main): add hive init and hive adapters to the main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:hive_ce/hive.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:team_5_examapp/config/di/di.dart';
+import 'package:team_5_examapp/features/questions/data/models/answer_dto.dart';
+import 'package:team_5_examapp/features/questions/data/models/check_answer_dto.dart';
 import 'core/routing/routes_manager.dart';
 import 'core/themes/light_theme.dart';
 import 'generated/l10n.dart';
@@ -11,6 +15,11 @@ void main() async {
   // Avoid 0× screen scale before layout; otherwise `.sp` font sizes become 0 and StrutStyle asserts.
   await ScreenUtil.ensureScreenSize();
   configureDependencies();
+  final dir = await getApplicationDocumentsDirectory();
+
+  Hive.init(dir.path);
+  Hive.registerAdapter(AnswerDtoAdapter());
+  Hive.registerAdapter(CheckAnswerDtoAdapter());
 
   runApp(const MyApp());
 }
