3fdf2f87a736dfe517d716ab50e22c9942323cb3
Your Name <mohamedmedo2282004a@gmail.com>
2026-04-02 14:31:37 +0200

    fix(config):change models folder

---
+++ lib/config/models/auth_responses_shared_models/auth_response/auth_response.dart
@@ -0,0 +1,35 @@
+import 'package:json_annotation/json_annotation.dart';
+import 'package:team_5_examapp/config/models/auth_responses_shared_models/user_response_dto.dart';
+
+part 'auth_response.g.dart';
+
+@JsonSerializable()
+class AuthResponse {
+  @JsonKey(name: "message")
+  String message;
+  @JsonKey(name: "token")
+  String token;
+  @JsonKey(name: "user")
+  UserResponseDto userResponseDto;
+
+  AuthResponse({
+    required this.message,
+    required this.token,
+    required this.userResponseDto,
+  });
+
+  AuthResponse copyWith({
+    String? message,
+    String? token,
+    UserResponseDto? userResponseDto,
+  }) => AuthResponse(
+    message: message ?? this.message,
+    token: token ?? this.token,
+    userResponseDto: userResponseDto ?? this.userResponseDto,
+  );
+
+  factory AuthResponse.fromJson(Map<String, dynamic> json) =>
+      _$AuthResponseFromJson(json);
+
+  Map<String, dynamic> toJson() => _$AuthResponseToJson(this);
+}
