4c037013a3ff34fcb0154d068241b9848a97617f
Yasser Samir <yasoo976098@gmail.com>
2026-04-12 17:13:04 +0200

    hotfix(core): separate constants and hive keys to different files

--- lib/core/constants/contants_manager.dart
+++ lib/core/constants/contants_manager.dart
@@ -2,8 +2,3 @@ abstract class AppConstants {
   static const double scoreBarWidth = 0.12;
   static const double scoreBarAngle = 280;
 }
-
-abstract class HiveKeys {
-  static const String answersBoxName = 'answersBox';
-  static const String checkAnswersBoxName = 'checkAnswersBox';
-}
